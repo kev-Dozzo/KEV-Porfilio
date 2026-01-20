7483d34b2a702f82855df57626263cbce63f8648
ngalle <ngalleauxence@gmail.com>
2026-01-20 08:13:25 +0100

    portFolio 1 commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 const menu = document.getElementById("mobile-menu");
 const manulink = document.querySelector(".nav-menu");
+const carrouselTrack = document.querySelector('.project-content');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const cards = document.querySelectorAll('.project-card')
 
+
+//toggle nav
 menu.addEventListener("click", function () {
     menu.classList.toggle("is-active");
     manulink.classList.toggle("active")
+})
+
+
+//carrousel
+
+let currentIdex = 0;
+
+const cardWith = cards[0].offsetWidth + 20; //card with + margin
+
+nextBtn.addEventListener('click', ()=>{
+    if(currentIdex < cards.length - 1 ){
+        currentIdex++
+        carrouselTrack.style.transform=`translateX(-${currentIdex * cardWith}px)`;
+    }
+})
+
+prevBtn.addEventListener('click', ()=>{
+    if(currentIdex >0){
+        currentIdex--;
+        carrouselTrack.style.transform=`translateX(-${currentIdex* cardWith}px)`;
+    }
 })
